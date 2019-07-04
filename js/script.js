402cfb488144107807f393d6fe1940f5c797ae45
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-07-05 01:06:28 +0200

    Fixed bugs and added missing code + deleted comments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', function() {
-  var board = {
-    name: 'Kanban Board',
-    addColumn: function(column) {
-      this.element.appendChild(column.element);
-      initSortable(column.id); //About this feature we will tell later
-    },
-    element: document.querySelector('#board .column-container')
-  };
+ function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+};
 
+function generateTemplate(name, data, basicElement) {
+  var template = document.getElementById(name).innerHTML;
+  var element = document.createElement(basicElement || 'div');
 
+  Mustache.parse(template);
+  element.innerHTML = Mustache.render(template, data);
 
-
-  // "classes"
-  function Column(name) {
+  return element;
+}
+function Column(name) {
   var self = this;
 
   this.id = randomString();
@@ -20,90 +26,85 @@ document.addEventListener('DOMContentLoaded', function() {
   this.element = generateTemplate('column-template', { name: this.name, id: this.id });
 
   this.element.querySelector('.column').addEventListener('click', function (event) {
-      if (event.target.classList.contains('btn-delete')) {
-        self.removeColumn();
-      }
+    if (event.target.classList.contains('btn-delete')) {
+      self.removeColumn();
+    }
 
-      if (event.target.classList.contains('add-card')) {
-        self.addCard(new Card(prompt("Enter the name of the card")));
-      }
-    });
-  }
+    if (event.target.classList.contains('add-card')) {
+      self.addCard(new Card(prompt("Enter the name of the card")));
+    }
+  })
+}
 
-  Column.prototype = {
+Column.prototype = {
     addCard: function(card) {
       this.element.querySelector('ul').appendChild(card.element);
     },
     removeColumn: function() {
       this.element.parentNode.removeChild(this.element);
     }
-  };
+}
 
-  // functions
-  function randomString() {
-    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
-    var str = '';
-    for (var i = 0; i < 10; i++) {
-        str += chars[Math.floor(Math.random() * chars.length)];
+function Card(description) {
+  var self = this;
+
+  this.id = randomString();
+  this.description = description;
+  this.element = generateTemplate('card-template', { description: this.description }, 'li');
+
+  this.element.querySelector('.card').addEventListener('click', function (event) {
+    event.stopPropagation();
+
+    if (event.target.classList.contains('btn-delete')) {
+      self.removeCard();
     }
-    return str;
-  }
-  function generateTemplate(name, data, basicElement) {
-    var template = document.getElementById(name).innerHTML;
-    var element = document.createElement(basicElement || 'div');
+  })
+}
 
-    Mustache.parse(template);
-    element.innerHTML = Mustache.render(template, data);
+Card.prototype = {
+	removeCard: function() {
+		this.element.parentNode.removeChild(this.element);
+    }
+}
 
-    return element;
-  }
-  function Card(description) {
-    var self = this;
+var board = {
+    name: 'Kanban Board',
+    addColumn: function(column) {
+      this.element.appendChild(column.element);
+      initSortable(column.id); //About this feature we will tell later
+    },
+    element: document.querySelector('#board .column-container')
+}
 
-    this.id = randomString();
-    this.description = description;
-    this.element = generateTemplate('card-template', { description: this.description }, 'li');
+function initSortable(id) {
+  var el = document.getElementById(id);
+  var sortable = Sortable.create(el, {
+    group: 'kanban',
+    sort: true
+  });
+}
 
-    this.element.querySelector('.card').addEventListener('click', function (event) {
-      event.stopPropagation();
+document.querySelector('#board .create-column').addEventListener('click', function() {
+    var name = prompt('Enter a column name');
+    var column = new Column(name);
+    board.addColumn(column);
+})
 
-      if (event.target.classList.contains('btn-delete')) {
-        self.removeCard();
-      }
-    });
-  }
-  // drag and drop
-  function initSortable(id) {
-    var el = document.getElementById(id);
-    var sortable = Sortable.create(el, {
-      group: 'kanban',
-      sort: true
-    });
-  }
-        // drag and drop event
-        document.querySelector('#board .create-column').addEventListener('click', function() {
-          var name = prompt('Enter a column name');
-          var column = new Column(name);
-          board.addColumn(column);
-      });
+// CREATING COLUMNS
+var todoColumn = new Column('To do');
+var doingColumn = new Column('Doing');
+var doneColumn = new Column('Done');
 
-      // Creat Fundamental Elements
-      // CREATING COLUMNS
-      var todoColumn = new Column('To do');
-      var doingColumn = new Column('Doing');
-      var doneColumn = new Column('Done');
+// ADDING COLUMNS TO THE BOARD
+board.addColumn(todoColumn);
+board.addColumn(doingColumn);
+board.addColumn(doneColumn);
 
-      // ADDING COLUMNS TO THE BOARD
-      board.addColumn(todoColumn);
-      board.addColumn(doingColumn);
-      board.addColumn(doneColumn);
+// CREATING CARDS
+var card1 = new Card('New task');
+var card2 = new Card('Create kanban boards');
 
-      // CREATING CARDS
-      var card1 = new Card('New task');
-      var card2 = new Card('Create kanban boards');
-
-      // ADDING CARDS TO COLUMNS
-      todoColumn.addCard(card1);
-      doingColumn.addCard(card2);
-
+// ADDING CARDS TO COLUMNS
+todoColumn.addCard(card1);
+doingColumn.addCard(card2);
 });
